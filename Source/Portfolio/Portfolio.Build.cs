f5f4568e052b3a8133ef92d777e47b98d63c99d6
Younghan <rsfnqf98@naver.com>
2024-05-12 08:40:46 +0900

    Pawn을 활용한 Character 클래스 연습 생성

--- Source/Portfolio/Portfolio.Build.cs
+++ Source/Portfolio/Portfolio.Build.cs
@@ -7,7 +7,12 @@ public class Portfolio : ModuleRules
 	public Portfolio(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
+		PublicIncludePaths.AddRange(new string[]
+		{
+			"Portfolio"
+		});
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
